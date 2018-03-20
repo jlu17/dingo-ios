bcfdd4c57d18ecf344330df2b132f8db85134a10
Jennifer Lu <jennifer.z.lu@gmail.com>
2018-03-19 19:34:15 -0700

    added empty XCodeProject

---
+++ dingo/dingo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dingo
+//
+//  Created by Jennifer Lu on 3/19/18.
+//  Copyright © 2018 Jennifer Lu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
